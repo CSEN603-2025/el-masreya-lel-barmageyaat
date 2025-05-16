bf6fc8f15f2625ddaea15f25e5c84b17a5ec3ba5
nourhaann <mnourhan86@gmail.com>
2025-05-16 19:51:11 +0300

    frontupdate

--- src/components/studentsNavBar/StudentsNavBar.jsx
+++ src/components/studentsNavBar/StudentsNavBar.jsx
@@ -1,56 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import StudentNotifications from "../StudentNotifications/StudentNotifications";
-import "./StudentsNavBar.css"; // Make sure this line is added
+import "./StudentsNavBar.css";
 
 function StudentsNavBar({ currUser, notifications, onMarkAsRead, onClearAll }) {
   return (
     <div>
       <nav className="navbar">
         <div className="navbar-container">
+
+          {/* Logo Section */}
+          <div className="navbar-logo">
+            <Link to="/">
+              <img src="/src/assets/scadbluelogol.jpg" alt="Logo" className="logo-img" />
+            </Link>
+          </div>
+
+          {/* Links */}
           <ul className="navbar-links">
             <li>
-              <Link to="/" className="navbar-link">
-                Home
-              </Link>
+              <Link to="/" className="navbar-link">Home</Link>
             </li>
             <li>
-              <Link to="/studentsDashboard" className="navbar-link">
-                Dashboard
-              </Link>
+              <Link to="/studentsDashboard" className="navbar-link">Dashboard</Link>
             </li>
             <li>
-              <Link to="/studentProfile" className="navbar-link">
-                Profile
-              </Link>
+              <Link to="/studentProfile" className="navbar-link">Profile</Link>
             </li>
             <li>
-              <Link to="/login" className="navbar-link">
-                Logout
-              </Link>
+              <Link to="/login" className="navbar-link">Logout</Link>
             </li>
             <li>
-              <Link to="/CompanyRegister" className="navbar-link">
-                Company Register
-              </Link>
+              <Link to="/CompanyRegister" className="navbar-link">Company Register</Link>
             </li>
             <li>
-              <Link to="/ViewCompanyRequest" className="navbar-link">
-                View Company Request
-              </Link>
+              <Link to="/ViewCompanyRequest" className="navbar-link">View Company Request</Link>
             </li>
             <li>
-              <Link to="/CompanyViewPostings" className="navbar-link">
-                Company View Postings
-              </Link>
+              <Link to="/CompanyViewPostings" className="navbar-link">Company View Postings</Link>
             </li>
             <li>
-              <Link to="/StudentsViewApplications" className="navbar-link">
-                Internship Application
-              </Link>
+              <Link to="/StudentsViewApplications" className="navbar-link">Internship Application</Link>
             </li>
           </ul>
-          
+
+          {/* Notifications */}
           {currUser && (
             <div className="navbar-notifications">
               <StudentNotifications 
